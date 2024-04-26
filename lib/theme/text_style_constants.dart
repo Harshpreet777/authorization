eed710397efcaaf02aaadff31dcf297169968649
Deeksha Sharma <deeksha.sharma.ctr@compositeapps.net>
2024-04-26 14:59:46 +0530

    # font constant created

--- lib/theme/text_style_constants.dart
+++ lib/theme/text_style_constants.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/constants/font_constant.dart';
 import 'package:flutter_boilerplate/theme/app_color.dart';
 
 class TextStyleConstants {
   TextStyle extraSmallTextStyle =
-      TextStyle(fontFamily: 'blisspro', fontSize: 10, color: AppColor().black);
+      TextStyle(fontFamily: FontConstants().baseFont, fontSize: FontConstants().light, color: AppColor().black);
 
   TextStyle smallTextStyle() {
     return TextStyle(
-        fontFamily: 'blisspro', fontSize: 12, color: AppColor().black);
+        fontFamily: FontConstants().baseFont, fontSize: FontConstants().small, color: AppColor().black);
   }
 
   TextStyle mediumTextStyle() {
     return TextStyle(
-        fontFamily: 'blisspro', fontSize: 14, color: AppColor().black);
+        fontFamily: FontConstants().baseFont, fontSize: FontConstants().medium, color: AppColor().black);
   }
 
   TextStyle largeTextStyle() {
     return TextStyle(
-        fontFamily: 'blisspro', fontSize: 16, color: AppColor().black);
+        fontFamily: FontConstants().baseFont, fontSize: FontConstants().large, color: AppColor().black);
   }
 
   TextStyle extraLargeTextStyle() {
     return TextStyle(
-        fontFamily: 'blisspro', fontSize: 20, color: AppColor().black);
+        fontFamily: FontConstants().baseFont, fontSize: FontConstants().extraLarge, color: AppColor().black);
   }
 }
